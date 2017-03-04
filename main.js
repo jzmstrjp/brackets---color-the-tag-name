5e6e3bb0b9a98201218ba0235cf0ff1137c97e47
jzmstrjp <jazzm@ster.jp.net>
2017-03-04 19:54:53 +0900

    6
    6

--- main.js
+++ main.js
@@ -1,44 +1,60 @@
 define(function (require, exports, module) {
+    "use strict";
+    
+    // Brackets modules
+    var EditorManager   = brackets.getModule("editor/EditorManager"),
+        AppInit         = brackets.getModule("utils/AppInit"),
+        MainViewManager = brackets.getModule("view/MainViewManager"),
+        ExtensionUtils  = brackets.getModule("utils/ExtensionUtils");
+    
+    ExtensionUtils.loadStyleSheet(module, "main.css");
+  
+    var tags;
+    var able = true;
+    var timer = false;
+    
+    
+    function timer_func(){
+        if(able){
+            action(100);
+            able = false;
+        }
+        if(!timer){
+            timer = setTimeout(function(){
+                able = true;
+                timer = false;
+            }, 100);
+        }
+    }
+    
+    function action(time){
+        setTimeout(tag_color_change, time);
+    }
 
-	'use strict';
+    function tag_color_change() {
+        if(!tags){
+            tags = document.getElementById("editor-holder").getElementsByClassName("cm-tag");
+            //tags = document.querySelectorAll(".cm-tag:not(.cm-bracket)");
+        }
+        [].forEach.call(tags, function (elm) {
+            elm.setAttribute("data-tag-name", elm.innerHTML);
+        });
+    }
+    
+    function updateUI() {
+        var editor  = EditorManager.getCurrentFullEditor();
+        $(editor).on("scroll keyEvent cursorActivity", timer_func);
+        action(300);
+    }
 
-	var AppInit = brackets.getModule('utils/AppInit'),
-		EditorManager = brackets.getModule('editor/EditorManager'),
-		MainViewManager = brackets.getModule('view/MainViewManager'),
-		ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
 
-	var able = true;
-	var timer = false;
-	
-	ExtensionUtils.loadStyleSheet(module, "main.css");
-
-	AppInit.appReady(function () {
-		$(document).on("click mousewheel keyup", timer_func);
-	});
-	
-	function timer_func(){
-		if(able){
-			action();
-			able = false;
-		}
-		if(!timer){
-			timer = setTimeout(function(){
-				able = true;
-				timer = false;
-			}, 100);
-		}
-	}
-	
-	function action(){
-		setTimeout(tag_color_change, 100);
-	}
-
-	function tag_color_change() {
-		var tags = document.querySelectorAll(".cm-tag:not(.cm-bracket)");
-		console.log("tag_color_change");
-		[].forEach.call(tags, function (elm) {
-			elm.setAttribute("data-tag-name", elm.innerHTML);
-		});
-	}
-
+    // Initialize extension
+    AppInit.appReady(function () {
+        MainViewManager.on("currentFileChange", updateUI);
+        action(3000);
+    });
 });
+
+
+
+
