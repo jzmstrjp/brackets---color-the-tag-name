739754dd53cdd8aac5b2eb9c76a78b91aaa2e592
jzmstrjp <jazzm@ster.jp.net>
2017-03-19 21:46:51 +0900

    ver3
    ver3

--- main.js
+++ main.js
@@ -1,56 +1,55 @@
-define(function (require, exports, module) {
-	"use strict";
+define(function(require, exports, module) {
+    "use strict";
 
-	// Brackets modules
-	var EditorManager = brackets.getModule("editor/EditorManager"),
-		AppInit = brackets.getModule("utils/AppInit"),
-		MainViewManager = brackets.getModule("view/MainViewManager"),
-		ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+    // Brackets modules
+    var EditorManager = brackets.getModule("editor/EditorManager"),
+        AppInit = brackets.getModule("utils/AppInit"),
+        MainViewManager = brackets.getModule("view/MainViewManager"),
+        ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
 
 	ExtensionUtils.loadStyleSheet(module, "main.css");
 
 	var cmTag;
+    var overlay = {
+        token: function(stream, state) {
+            var ch;
+            if (stream.match(/<(\/|)/)) {
+                return "open-bracket";
+            } else if (stream.match(/(\/|)>/)) {
+                return "close-bracket";
+            }
+            while (stream.next() != null && !stream.match(/<(\/|)|(\/|)>/, false)) {}
+            return null;
+        }
+    };
 
-	function tag_color_change() {
-		//console.log("発火");
-		/*var cmBracket = document.getElementById("editor-holder").querySelectorAll(".cm-bracket");
-		Array.prototype.forEach.call(cmBracket, function (elm, i, arr) {
-			var gt = elm.innerHTML.indexOf('&gt;');
-			var lt = elm.innerHTML.indexOf('&lt;');
-			if (gt !== -1 && lt !== -1 && elm.getAttribute("data-cloned") != "true"){
-				elm.setAttribute("data-cloned", "true");
-				var newElm = $(elm).clone(true);
-				console.log("gt"+gt);
-				console.log("lt"+lt);
-				newElm[0].innerHTML = newElm[0].innerHTML.slice(0, gt+3);
-				elm.innerHTML = elm.innerHTML.slice(lt);
-				newElm.insertBefore(elm);
-			}
-		});*/
+    
 
-		Array.prototype.forEach.call(cmTag, function (elm, i, arr) {
-			if(!elm.classList.contains("cm-bracket")){
-				/*if(arr[i-1].classList.contains("cm-bracket") && arr[i-1].innerHTML.indexOf('&lt;') !== -1){
-					arr[i-1].setAttribute("data-tag-name", elm.innerHTML);
-				}*/
-				elm.setAttribute("data-tag-name", elm.innerHTML);
-				/*if(arr[i+1] && arr[i+1].classList.contains("cm-bracket")){
-					arr[i+1].setAttribute("data-tag-name", elm.innerHTML);
-				}*/
-			}
-		});
-	}
+    function tag_color_change() {
+        Array.prototype.forEach.call(cmTag, function(elm, i, arr) {
+            if (!elm.classList.contains("cm-bracket")) {
+                if (arr[i - 1].classList.contains("cm-open-bracket")) {
+                    arr[i - 1].setAttribute("data-tag-name", elm.innerHTML);
+                }
+                elm.setAttribute("data-tag-name", elm.innerHTML);
+                if (arr[i + 1] && arr[i + 1].classList.contains("cm-close-bracket")) {
+                    arr[i + 1].setAttribute("data-tag-name", elm.innerHTML);
+                }
+            }
+        });
+    }
 
-	function updateUI() {
-		cmTag = document.getElementById("editor-holder").getElementsByClassName("cm-tag");
-		var editor = EditorManager.getCurrentFullEditor();
-		var cm = editor._codeMirror;
-		cm.on("update", tag_color_change);
-	}
+    function updateUI() {
+        cmTag = document.getElementById("editor-holder").getElementsByClassName("cm-tag");
+        var editor = EditorManager.getCurrentFullEditor();
+        var cm = editor._codeMirror;
+        cm.removeOverlay(overlay);
+        cm.addOverlay(overlay);
+        cm.on("update", tag_color_change);
+    }
 
-
-	// Initialize extension
-	AppInit.appReady(function () {
-		MainViewManager.on("currentFileChange", updateUI);
-	});
+    // Initialize extension
+    AppInit.appReady(function() {
+        MainViewManager.on("currentFileChange", updateUI);
+    });
 });
